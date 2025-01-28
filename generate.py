317cee0c12d1b96c70ece6ad0d7a7efe6633eb95
Joseph Cancellieri <Joseph.Cancellieri@uvm.edu>
2025-01-27 20:41:59 -0500

    Completed "Many Links"

--- generate.py
+++ generate.py
@@ -10,8 +10,22 @@ x, y, z = 0, 0, 0.5
 
 #pyrosim.Send_Cube(name="Box2", pos=[x, y, z] , size=[length, width, height])
 
+'''
 for i in range(10):
     pyrosim.Send_Cube(name="Tower", pos=[x, y, (z + i)], size=[length, width, height])
     length, width, height = length * 0.9, width * 0.9, height * 0.9
+'''
+size = 1
 
+for i in range(5):
+    for i in range(5):
+        for i in range(10):
+            pyrosim.Send_Cube(name="Tower", pos=[x, y, (z + i)], size=[size, size, size])
+            size = size * 0.9
+
+        y = y + 1
+        size = 1
+        
+    x = x + 1
+    y = 0
 pyrosim.End()
