a9cdff4f107d5b4e22fbf7908102ee4755a288e8
Joseph Cancellieri <Joseph.Cancellieri@uvm.edu>
2025-01-28 10:41:52 -0500

    Completed "A two-link, one-joint robot."

--- generate.py
+++ generate.py
@@ -39,7 +39,11 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
 
-    pyrosim.Send_Cube(name="Torso", pos=[x, y, z] , size=[length, width, height])
+    pyrosim.Send_Cube(name="Torso", pos=[x, y, z], size=[length, width, height])
+
+    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [x + 0.5, y, z + 1])
+
+    pyrosim.Send_Cube(name="Leg", pos=[x + 1, y, z + 1], size=[length, width, height])
 
     pyrosim.End()
 
