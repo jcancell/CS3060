b49e90ebc2a28b91641385d191d078e88c30c1c4
Joseph Cancellieri <Joseph.Cancellieri@uvm.edu>
2025-02-10 14:39:31 -0500

    Completed "Adding a sensor."

--- simulate.py
+++ simulate.py
@@ -1,6 +1,8 @@
 import pybullet_data
 import pybullet as p
 import time as t
+import pyrosim.pyrosim as pyrosim
+
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
@@ -10,8 +12,13 @@ robotId = p.loadURDF("body.urdf")
 
 p.loadSDF("world.sdf")
 
+pyrosim.Prepare_To_Simulate(robotId)
+
 for x in range(100000):
     p.stepSimulation()
+
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+
     t.sleep(1/60)
     print(x)
 p.disconnect()
