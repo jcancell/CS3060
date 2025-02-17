293ee1f45c4ab6ce5116ad8375baedb368e37e81
Joseph Cancellieri <Joseph.Cancellieri@uvm.edu>
2025-02-17 14:58:19 -0500

    Open-loop control of movement.

--- analyze.py
+++ analyze.py
@@ -1,17 +1,32 @@
 import numpy
 import matplotlib.pyplot
 
-backLegSensorValues = numpy.load('data/backLegSensorValues.npy')
+#backLegSensorValues = numpy.load('data/backLegSensorValues.npy')
 
 #print(backLegSensorValues)
 
-matplotlib.pyplot.plot(backLegSensorValues, label="Back Leg", linewidth=5)
+#matplotlib.pyplot.plot(backLegSensorValues, label="Back Leg", linewidth=5)
 
-frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy')
+#frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy')
 
 #print(backLegSensorValues)
 
-matplotlib.pyplot.plot(frontLegSensorValues, label="Front Leg", linewidth=2.5)
+#matplotlib.pyplot.plot(frontLegSensorValues, label="Front Leg", linewidth=2.5)
+
+
+#targetAngles = numpy.load('data/targetAngles.npy')
+
+#matplotlib.pyplot.plot(targetAngles)
+
+
+backLegMotorValues = numpy.load('data/backLegMotorValues.npy')
+
+matplotlib.pyplot.plot(backLegMotorValues, label="Back Leg", linewidth=5)
+
+frontLegMotorValues = numpy.load('data/frontLegMotorValues.npy')
+
+matplotlib.pyplot.plot(frontLegMotorValues, label="Front Leg", linewidth=1.5)
+
 
 matplotlib.pyplot.legend()
 
--- simulate.py
+++ simulate.py
@@ -6,6 +6,16 @@ import numpy as np
 import random as rd
 
 SIM_STEPS = 1000
+# num seconds between loop iterations
+SIM_SPEED = 1/240
+
+frontLeg_amplitude = np.pi/3
+frontLeg_frequency = 10
+frontLeg_phaseOffset = 0
+
+backLeg_amplitude = np.pi/6
+backLeg_frequency = 5
+backLeg_phaseOffset = 0.5
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -20,6 +30,17 @@ pyrosim.Prepare_To_Simulate(robotId)
 
 backLegSensorValues = np.zeros(SIM_STEPS)
 frontLegSensorValues = np.zeros(SIM_STEPS)
+backLegMotorValues = np.zeros(SIM_STEPS)
+frontLegMotorValues = np.zeros(SIM_STEPS)
+
+vals = np.linspace(0, 2 * np.pi, SIM_STEPS)
+
+backLeg_targetAngles = [backLeg_amplitude * np.sin(backLeg_frequency * x + backLeg_phaseOffset)for x in vals]
+
+frontLeg_targetAngles = [frontLeg_amplitude * np.sin(frontLeg_frequency * x + frontLeg_phaseOffset)for x in vals]
+
+#np.save('data/targetAngles.npy', targetAngles)
+#exit()
 
 for x in range(SIM_STEPS):
     p.stepSimulation()
@@ -37,21 +58,27 @@ for x in range(SIM_STEPS):
         bodyIndex = robotId,
         jointName = b'Torso_BackLeg',
         controlMode = p.POSITION_CONTROL,
-        targetPosition = (rd.uniform(-np.pi/2, np.pi/2)),
+        targetPosition = backLeg_targetAngles[x],
         maxForce = 50)
     
+    backLegMotorValues[x] = backLeg_targetAngles[x]
+
     # Front Leg Motor
     pyrosim.Set_Motor_For_Joint(
         bodyIndex = robotId,
         jointName = b'Torso_FrontLeg',
         controlMode = p.POSITION_CONTROL,
-        targetPosition = (rd.uniform(-np.pi/2, np.pi/2)),
+        targetPosition = frontLeg_targetAngles[x],
         maxForce = 50)
+    
+    frontLegMotorValues[x] = frontLeg_targetAngles[x]
 
-    t.sleep(1/60)
+    t.sleep(SIM_SPEED)
     #print(x)
 p.disconnect()
 
 np.save('data/backLegSensorValues.npy', backLegSensorValues)
 np.save('data/frontLegSensorValues.npy', frontLegSensorValues)
+np.save('data/backLegMotorValues.npy', backLegMotorValues)
+np.save('data/frontLegMotorValues', frontLegMotorValues)
 #print(backLegSensorValues)
