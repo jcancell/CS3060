3b1aeccd084a439e4882eac90d01d3b708788324
Joseph Cancellieri <Joseph.Cancellieri@uvm.edu>
2025-03-02 12:28:21 -0500

    Hidden and motor neurons.

--- pyrosim/neuralNetwork.py
+++ pyrosim/neuralNetwork.py
@@ -29,10 +29,12 @@ class NEURAL_NETWORK:
         print("")
 
     def Update(self):
-        for key in self.neurons:
-            if self.neurons[key].Is_Sensor_Neuron():
+        for neuronName in self.neurons:
+            if self.neurons[neuronName].Is_Sensor_Neuron():
 
-                self.neurons[key].Update_Sensor_Neuron()
+                self.neurons[neuronName].Update_Sensor_Neuron()
+            else:
+                self.neurons[neuronName].Update_Hidden_Or_Motor_Neuron()
 
 
 
