94b7dc8ba126f02e169b7fbaa1d1ab537c068a31
Joseph Cancellieri <Joseph.Cancellieri@uvm.edu>
2025-03-20 10:26:48 -0400

    Creating the hill climber.

--- search.py
+++ search.py
@@ -1,6 +1,10 @@
 import os
-import pybullet as p
+import hillclimber
 
+'''
 for i in range(5):
     os.system("py generate.py")
     os.system("py simulate.py")
+'''
+
+hc = hillclimber.HILL_CLIMBER()
