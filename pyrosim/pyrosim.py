21683103a2e48a044fa917ae6ab957c7d788e087
Joseph Cancellieri <Joseph.Cancellieri@uvm.edu>
2025-04-07 19:51:44 -0400

    Changing Joint Axes

--- pyrosim/pyrosim.py
+++ pyrosim/pyrosim.py
@@ -134,11 +134,11 @@ def Send_Cube(name="default",pos=[0,0,0],size=[1,1,1]):
 
     availableLinkIndex = availableLinkIndex + 1
 
-def Send_Joint(name,parent,child,type,position):
+def Send_Joint(name,parent,child,type,position,jointAxis): #Step 28
 
     joint = JOINT(name,parent,child,type,position)
 
-    joint.Save(f)
+    joint.Save(f,jointAxis) #Step 27
 
 def Send_Motor_Neuron(name,jointName):
 
