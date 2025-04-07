ac6b88c86811b9b0b7dfa87c8c2334d1c1946bea
Joseph Cancellieri <Joseph.Cancellieri@uvm.edu>
2025-04-07 15:38:59 -0400

    The torso.

--- solution.py
+++ solution.py
@@ -54,7 +54,7 @@ class SOLUTION:
     def Create_Body(self):
         pyrosim.Start_URDF("body.urdf")
 
-        pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[length, width, height])
+        pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0], size=[length, width, height])
 
         pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [2, 0, 1])
 
