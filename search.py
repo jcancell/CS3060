8ce65087d7c62ea74e9571038cedf1e64328f5c2
Joseph Cancellieri <Joseph.Cancellieri@uvm.edu>
2025-04-21 13:50:28 -0400

    A/B Mass Dist.

--- search.py
+++ search.py
@@ -1,10 +1,19 @@
 import os
 import hillclimber
 import parallelHillClimber
+import sys
 
 #hc = hillclimber.HILL_CLIMBER()
 #hc.Evolve()
 #hc.Show_Best()
+
+if len(sys.argv) != 2 or sys.argv[1] not in ['A', 'B']:
+    print("Usage: python search.py A|B")
+    exit(1)
+
+experiment = sys.argv[1]
+os.environ['EXPERIMENT_VERSION'] = experiment  # Pass it as an environment variable
+
 
 phc = parallelHillClimber.PARALLEL_HILL_CLIMBER()
 phc.Evolve()
--- solution.py
+++ solution.py
@@ -7,6 +7,8 @@ import constraints
 
 length, width, height = 1, 1, 1
 
+exp_version = os.environ.get('VERSION')
+
 class SOLUTION:
 
     def __init__(self, nextAvailableID):
@@ -54,48 +56,58 @@ class SOLUTION:
     def Create_Body(self):
         pyrosim.Start_URDF("body.urdf")
 
+        if exp_version == 'A':
+            print("Version A")
+            mass_torso = 4.0
+            mass_upper_leg = 0.5
+            mass_lower_leg = 1.0
+        else:
+            print("Version B")
+            mass_torso = 0.5
+            mass_upper_leg = 2.0
+            mass_lower_leg = 4.0
 
-        pyrosim.Send_Cube(name="Torso", pos=[0, 0, 4], size=[length, width, height],mass=1.0)
+        pyrosim.Send_Cube(name="Torso", pos=[0, 0, 4], size=[length, width, height],mass=mass_torso)
 
 
         pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0, 0.5, 4] , jointAxis = "1 0 0")
 
-        pyrosim.Send_Cube(name="FrontLeg", pos=[0, 0.5, 0], size=[0.2, 1, 0.2],mass=1.0)
+        pyrosim.Send_Cube(name="FrontLeg", pos=[0, 0.5, 0], size=[0.2, 1, 0.2],mass=mass_upper_leg)
 
 
         pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0, -0.5, 4] , jointAxis = "1 0 0")
 
-        pyrosim.Send_Cube(name="BackLeg", pos=[0, -0.5, 0], size=[0.2, 1, 0.2],mass=1.0)
+        pyrosim.Send_Cube(name="BackLeg", pos=[0, -0.5, 0], size=[0.2, 1, 0.2],mass=mass_upper_leg)
 
 
         pyrosim.Send_Joint( name = "Torso_LeftLeg" , parent= "Torso" , child = "LeftLeg" , type = "revolute", position = [-0.5, 0, 4] , jointAxis = "0 1 0")
 
-        pyrosim.Send_Cube(name="LeftLeg", pos=[-0.5, 0, 0], size=[1, 0.2, 0.2],mass=1.0)
+        pyrosim.Send_Cube(name="LeftLeg", pos=[-0.5, 0, 0], size=[1, 0.2, 0.2],mass=mass_upper_leg)
 
 
         pyrosim.Send_Joint(name="Torso_RightLeg", parent="Torso", child="RightLeg", type="revolute", position=[0.5, 0, 4],jointAxis="0 1 0")
         
-        pyrosim.Send_Cube(name="RightLeg", pos=[0.5, 0, 0], size=[1,0.2,0.2],mass=1.0)
+        pyrosim.Send_Cube(name="RightLeg", pos=[0.5, 0, 0], size=[1,0.2,0.2],mass=mass_upper_leg)
 
 
         pyrosim.Send_Joint(name="FrontLeg_FrontLowerLeg", parent="FrontLeg", child="FrontLowerLeg", type="revolute", position=[0, 1, 0], jointAxis="1 0 0")
 
-        pyrosim.Send_Cube(name="FrontLowerLeg", pos=[0, 0, -.5], size=[0.2, 0.2, 1],mass=1.0)
+        pyrosim.Send_Cube(name="FrontLowerLeg", pos=[0, 0, -.5], size=[0.2, 0.2, 1],mass=mass_lower_leg)
 
 
         pyrosim.Send_Joint(name="BackLeg_BackLowerLeg", parent="BackLeg", child="BackLowerLeg", type="revolute", position=[0, -1, 0], jointAxis="1 0 0")
 
-        pyrosim.Send_Cube(name="BackLowerLeg", pos=[0, 0, -.5], size=[0.2, 0.2, 1],mass=1.0)
+        pyrosim.Send_Cube(name="BackLowerLeg", pos=[0, 0, -.5], size=[0.2, 0.2, 1],mass=mass_lower_leg)
 
 
         pyrosim.Send_Joint(name="LeftLeg_LeftLowerLeg", parent="LeftLeg", child="LeftLowerLeg", type="revolute", position=[-1, 0, 0], jointAxis="1 0 0")
 
-        pyrosim.Send_Cube(name="LeftLowerLeg", pos=[0, 0, -.5], size=[0.2, 0.2, 1],mass=1.0)
+        pyrosim.Send_Cube(name="LeftLowerLeg", pos=[0, 0, -.5], size=[0.2, 0.2, 1],mass=mass_lower_leg)
 
 
         pyrosim.Send_Joint(name="RightLeg_RightLowerLeg", parent="RightLeg", child="RightLowerLeg", type="revolute", position=[1, 0, 0], jointAxis="1 0 0")
 
-        pyrosim.Send_Cube(name="RightLowerLeg", pos=[0, 0, -.5], size=[0.2, 0.2, 1],mass=1.0)
+        pyrosim.Send_Cube(name="RightLowerLeg", pos=[0, 0, -.5], size=[0.2, 0.2, 1],mass=mass_lower_leg)
 
         pyrosim.End()
         #exit() #Step 30
